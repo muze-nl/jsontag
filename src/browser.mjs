b803196b69704ef77ac26c4e9aafe079f88b02b5
Auke van Slooten <auke@muze.nl>
2022-04-18 09:47:25 +0200

    added browser build

---
+++ src/browser.mjs
@@ -0,0 +1,24 @@
+import UUID from './lib/UUID.mjs'
+import Decimal from './lib/Decimal.mjs'
+import Money from './lib/Money.mjs'
+import Null from './lib/Null.mjs'
+import * as attr from './lib/functions.mjs'
+import parse from './lib/parse.mjs'
+
+window.TSON = {
+	UUID: UUID,
+	Decimal: Decimal,
+	Money: Money,
+	Null: Null,
+	stringify: attr.stringify,
+	parse: parse,
+	getType: attr.getType,
+	getTypeString: attr.getTypeString,
+	setAttribute: attr.setAttribute,
+	getAttribute: attr.getAttribute,
+	addAttribute: attr.addAttribute,
+	getAttributes: attr.getAttributes,
+	setAttributes: attr.setAttributes,
+	getAttributesString: attr.getAttributesString,
+	isNull: attr.isNull
+}
