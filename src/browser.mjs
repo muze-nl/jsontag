e08e19114323a5e62dce2c94389f29e7699abc03
Auke van Slooten <auke@muze.nl>
2024-03-22 11:53:54 +0100

    added missing clone and Link

--- src/browser.mjs
+++ src/browser.mjs
@@ -1,14 +1,16 @@
 import Null from './lib/Null.mjs'
+import Link from './lib/Link.mjs'
 import * as attr from './lib/functions.mjs'
 import parse from './lib/fast-parse.mjs'
 
 window.JSONTag = {
-	Null: Null,
 	stringify: attr.stringify,
-	parse: parse,
+	parse,
+
 	getType: attr.getType,
 	setType: attr.setType,
 	getTypeString: attr.getTypeString,
+
 	setAttribute: attr.setAttribute,
 	getAttribute: attr.getAttribute,
 	addAttribute: attr.addAttribute,
@@ -16,5 +18,10 @@ window.JSONTag = {
 	getAttributes: attr.getAttributes,
 	setAttributes: attr.setAttributes,
 	getAttributesString: attr.getAttributesString,
+
 	isNull: attr.isNull,
+	clone: attr.clone,
+
+	Link,
+	Null
 }
